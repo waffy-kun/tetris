557c39ac1472b8b71cb7c87ac17c0bbedf1abf5a
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-20 02:00:42 +0800

    Defined L-shape initial position

--- pieces.cpp
+++ pieces.cpp
@@ -239,5 +239,11 @@ int mPiecesInitialLocation [7 /* Types */][4 /* Rotations */][2 /* Position */] 
         {-2, -2},
         {-2, -3}
     },
-    
+    //L-shape
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
 };
