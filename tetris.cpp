1604f489b9b4faf71a3c9ef38bf4f4973e8a2354
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-19 14:51:36 +0800

    Declared 4d Array for Pieces

---
+++ tetris.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+char mPieces[7 /* types */]
+            [4 /* rotations */]
+            [5 /* horizontal blocks */]
+            [5 /* vertical blocks */] = {
+
+            };
