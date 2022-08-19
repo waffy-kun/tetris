87e14efad34c8fd48d70e6cf4aeb8f753ecd14f7
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-20 02:11:27 +0800

    Defined N-shape initial position

--- pieces.cpp
+++ pieces.cpp
@@ -253,4 +253,11 @@ int mPiecesInitialLocation [7 /* Types */][4 /* Rotations */][2 /* Position */] 
         {-2, -3},
         {-2, -3}
     },
+    //N-shape
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
 };
