3bb1dd531f5740b37882964e73a553165d55c268
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-20 02:12:45 +0800

    Defined mirrored N-shape initial position

--- pieces.cpp
+++ pieces.cpp
@@ -260,4 +260,11 @@ int mPiecesInitialLocation [7 /* Types */][4 /* Rotations */][2 /* Position */] 
         {-2, -3},
         {-2, -2}
     },
+    //Mirrored N-shape
+    {
+        {-2, -3},
+        {-2, -3},
+        {-2, -3},
+        {-2, -2}
+    },
 };
