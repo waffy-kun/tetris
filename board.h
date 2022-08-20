033b9f95a375664b5a443a77f0e2dfa9aecdcd6a
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-21 02:54:01 +0800

    Created new header file containing board constants

---
+++ board.h
@@ -0,0 +1,18 @@
+#ifndef _BOARD_
+#define _BOARD_
+
+//--INCLUDES--
+#include "pieces.h"
+
+//--DEFINES--
+
+#define BOARD_LINE_WIDTH 6 //WIDTH OF EACH TWO LINES WHICH DELIMIT BOARD
+#define BLOCK_SIZE 16 //Width and Height of each block piece
+#define BOARD_POSITION 320 //Center position from the leftmost part of screen
+#define BOARD_WIDTH 10 //Board Width in Blocks
+#define BOARD_HEIGHT 20 //Board Height in Blocks
+#define MIN_VERTICAL_MARGIN 20 //Minimum vertical margin for board limit
+#define MIN_HORIZONTAL_MARGIN 20 //Minimum horizontal margin for board limit
+#define PIECE_BLOCKS 5 //Number of horizontal and vertical blocks of matrix piece
+
+#endif // _BOARD_
