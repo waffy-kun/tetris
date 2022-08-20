bec1edaa9e1cd21dd4b2061da5322eb3bd303980
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-21 04:12:35 +0800

    Defined a method for storing piece to be drawn

--- board.cpp
+++ board.cpp
@@ -30,3 +30,28 @@ void Board::initBoard() {
         }
     }
 }
+
+/*
+======================
+Store a piece in the board by filling the blocks
+
+Parameters:
+
+>pX: Horizontal position in blocks
+>pY: Vertical position in blocks
+>pPiece: Piece to draw
+>pRotation: 1 of 4 possible rotations
+======================
+*/
+
+void Board::storePiece(int pX, int pY, int pPiece, int pRotation) {
+    //Store each piece of the block in the board
+    for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++) {
+        for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++) {
+            //Store only blocks that are not empty
+            if (mPieces -> getBlockType (pPiece, pRotation, j2, i2) != 0){
+                mBoard[i1][j1] = POS_FILLED;
+            }
+        }
+    }
+}
