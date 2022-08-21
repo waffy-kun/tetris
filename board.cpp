a3757a86f1d925f483bed9f136af096af0992768
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-22 01:12:04 +0800

    Added method that checks if piece can move freely

--- board.cpp
+++ board.cpp
@@ -137,3 +137,40 @@ Parameters:
 int Board::getYPosInPixels(int pPos) {
     return ((mScreenHeight - (BLOCK_SIZE * BOARD_HEIGHT)) + (pPos * BLOCK_SIZE));
 }
+
+/*
+==========================
+Check if piece can be store without collision in the board
+Returns true if movement is possible, else false
+
+Parameters:
+
+>pX: Horizontal position in blocks
+>pY: Vertical position in blocks
+>pPiece: Piece to draw
+>pRotation: 1 of 4 possible rotations
+==========================
+*/
+bool Board::isPossibleMove(int pX, int pY, int pPiece, int pRotation) {
+    //Checks collision with blocks already in the board or board limits
+    //Checks the 5 x 5 area of a piece with appropriate area in the board
+    for (int i1 = pX, i2 = 0; i1 < pX + PIECE_BLOCKS; i1++, i2++) {
+        for (int j1 = pY, j2 = 0; j1 < pY + PIECE_BLOCKS; j1++, j2++) {
+            //Check if piece went outside board limits
+            if (i1 < 0 ||
+                i1 > BOARD_WIDTH - 1 ||
+                j1 > BOARD_HEIGHT - 1) {
+                if (mPieces -> getBlockType(pPiece, pRotation, j2, i2) != 0) return 0;
+            }
+
+            //Check if a falling piece collide with a block already in the board
+            if (j1 >= 0) {
+                if ((mPieces -> getBlockType (pPiece, pRotation, j2, i2) != 0) && (!isFreeBlock(i1, j1))) {
+                    return false;
+                }
+            }
+        }
+    }
+    //No collision
+    return true;
+}
