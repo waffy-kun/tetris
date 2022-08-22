349668e40b725885863648775779d9c8b97a8fa2
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-22 18:21:48 +0800

    Created game.h file with includes and constants

---
+++ game.h
@@ -0,0 +1,13 @@
+#ifndef _GAME_
+#define _GAME_
+
+// ----- INCLUDES -----
+#include "board.h"
+#include "pieces.h"
+#include "io.h"
+#include <time.h>
+
+// ----- DEFINES -----
+#define WAIT_TIME 700 // Number of milliseconds before piece go 1 block down
+
+#endif
