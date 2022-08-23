789465a16f67b9a64026828e1b90868295d28250
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-23 15:13:31 +0800

    Defined initGame class for creation of game piece

--- game.cpp
+++ game.cpp
@@ -33,3 +33,21 @@ Parameters:
 int Game::getRand (int pA, int pB) {
     return rand() % (pB - pA + 1) + pB;
 }
+
+/*
+==========================
+Initial parameters of the game
+==========================
+*/
+void Game::initGame() {
+    // Init Random Numbers
+    srand ((unsigned int) time(NULL));
+
+    // First Piece
+    mPiece =    getRand(0, 6);
+    mRotation = getRand(0, 3);
+    mPosX =     (BOARD_WIDTH / 2) + mPieces -> getXInitialPosition (mPiece, mRotation);
+    mPosY =     mPieces -> getYInitialPosition (mPiece, mRotation);
+
+
+}
