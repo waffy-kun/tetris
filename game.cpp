839f065fc7ac6b4969d29e9d8b046756ed1ed03e
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-24 18:41:18 +0800

    Defined drawPiece method which will draw the piece

--- game.cpp
+++ game.cpp
@@ -73,3 +73,39 @@ void Game::createNewPiece() {
     mNextPiece =    getRand(0, 6);
     mNextRotation = getRand(0, 3);
 }
+
+/*
+============================
+Draw Piece
+
+Parameters:
+>pX: Horizontal position in blocks
+>pY: Vertical position in blocks
+>pPiece: Piece to draw
+>pRotation: 1 of 4 possible rotations
+============================
+*/
+void Game::drawPiece(int pX, int pY, int pPiece, int pRotation) {
+    color mColor; //Color of the block
+
+    //Obtain the position in pixel on the screen of the block to draw
+    int mPixelsX = mBoard -> getXPosInPixels (pX);
+    int mPixelsY = mBoard -> getYPosInPixels (pY);
+
+    //Travel the blocks matrix and draw filled blocks
+    for (int i = 0; i < PIECE_BLOCKS; i++) {
+        for (int j = 0; j < PIECE_BLOCKS; j++) {
+            // Get block type and draw with assigned color
+            switch (mPieces -> getBlockType (pPiece, pRotation, j, i)) {
+                case 1: mColor = GREEN; break; // For each block except pivot
+                case 2: mColor = BLUE; break; // For the pivot block
+            }
+            if (mPieces -> getBlockType (pPiece, pRotation, j, i) != 0) {
+                mIO -> drawRectangle (mPixelsX + i * BLOCK_SIZE, mPixelsY + j * BLOCK_SIZE,
+                                    (mPixelsX + i * BLOCK_SIZE) + BLOCK_SIZE - 1,
+                                    (mPixelsY + j * BLOCK_SIZE) + BLOCK_SIZE - 1,
+                                    mColor);
+            }
+        }
+    }
+}
