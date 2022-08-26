a8fc17ba7db7fbf0e274503bb48c58dcc3cbf066
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-26 19:07:53 +0800

    Created IO.h to handle game IO operations

---
+++ IO.h
@@ -0,0 +1,14 @@
+#ifndef _IO_
+#define _IO_
+
+// ------ INCLUDES ------
+#ifndef LINUX
+#include "SDL/include/SDL.h"
+#include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"
+#else
+#include "SDL/SDL.h"
+#include "SDL/SDL_GfxPrimitives/sdl_gfxprimitives.h"
+#endif
+#pragma comment (lib, "SDL/lib/SDL.lib")
+#pragma comment (lib, "SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
+
