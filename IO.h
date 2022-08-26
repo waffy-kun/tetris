18cefa8df3227d13cb061d5a4481eb5f873c663c
Raffy Wamar <wamarraffy154@gmail.com>
2022-08-26 19:14:29 +0800

    Added color enums and abstract methods

--- IO.h
+++ IO.h
@@ -12,3 +12,24 @@
 #pragma comment (lib, "SDL/lib/SDL.lib")
 #pragma comment (lib, "SDL/SDL_GfxPrimitives/SDL_GfxPrimitives_Static.lib")
 
+// ------ ENUMS ------
+enum color {BLACK, RED, GREEN, BLUE, CYAN, MAGENTA, YELLOW, WHITE, COLOR_MAX}; //colors
+
+// -------------------
+//  IO
+// -------------------
+class IO {
+    public:
+    IO();
+
+    void drawRectangle(int pX1, int pY1, int pX2, int pY2, enum color pC);
+    void clearScreen();
+    int getScreenHeight();
+    int initGraph();
+    int pollKey();
+    int getKey();
+    int isKeyDown();
+    void updateScreen();
+};
+
+#endif // _IO_
